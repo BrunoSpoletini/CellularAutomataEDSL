919e402ae6516e8da01081799996eab09b0283bc
BrunoSpoletini <bruno_spoletini@hotmail.com>
2024-02-29 14:40:19 -0300

    Basic UI Implemented

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,73 +3,96 @@ module Lib
     ) where
 
 import Data.IORef
+import Control.Monad
 
-import qualified Graphics.UI.Threepenny      as UI
+
+import           Graphics.UI.Threepenny      as UI
+import           Graphics.UI.Threepenny.Canvas as Canvas
 import           Graphics.UI.Threepenny.Core
+--import Distribution.Compat.Prelude
 
+cellSize = 25 
+canvasSize = 700 
+padding = 10  --padding
 
 startCA :: IO ()
 startCA = startGUI defaultConfig { jsStatic = Just "." } setup
+
+drawSquare :: Canvas -> Double -> Double -> Double -> String -> UI()
+drawSquare canvas x y size colour = do  canvas # set' UI.fillStyle (UI.htmlColor colour)
+                                        canvas # UI.fillRect (x,y) size size
+
+
+fromIntegralPoint :: (Int, Int) -> (Double, Double)
+fromIntegralPoint (x, y) =  (fromIntegral x, fromIntegral y)
 
 setup :: Window -> UI ()
 setup window = do
     UI.addStyleSheet window "foundation-5.css"
     UI.addStyleSheet window "grid.css"
     return  window # set UI.title "Cellular Automata"
-    getBody window #+ [ mkDisplay ]
-    return ()
 
-n = 5
 
-createGrid :: Int -> [[UI Element]]
-createGrid n = replicate n (replicate n ( UI.div #. "cell" ))
+    -- Canvas --
+    canvas <- UI.canvas #. "canvas"
+        # set UI.height canvasSize
+        # set UI.width  canvasSize
 
--- | Create counter element
-mkDisplay :: UI Element
-mkDisplay = do
-    grid    <- grid (createGrid n) #. "grid"
-    --testButton     <- UI.button # set UI.text "Testeo"
-    --bup     <- UI.button # set UI.text "Up"
-    --bdown   <- UI.button # set UI.text "Down"
-    --counter <- UI.div    # set UI.text "0"
+    -- Draw grid
+    forM_ [0,cellSize..canvasSize] $ \x -> do
+        UI.moveTo (fromIntegralPoint (x + padding + 1, padding)) canvas
+        UI.lineTo (fromIntegralPoint (x + padding + 1, canvasSize + padding)) canvas
+        
+        UI.moveTo (fromIntegralPoint (padding, x + padding + 1)) canvas
+        UI.lineTo (fromIntegralPoint (canvasSize + padding, x + padding + 1)) canvas
 
-    -- -- store counter value in an IORef
-    -- count <- liftIO $ newIORef 0
-    
-    -- -- join the two click events into a single event
-    -- let eclick :: Event (Int -> Int)
-    --     eclick = unionWith const
-    --         ( (+1)         <$ UI.click bup   )
-    --         ( (subtract 1) <$ UI.click bdown )
+    -- Buttons
+    clear     <- UI.button #+ [string "Clear"]
+    drawRects <- UI.button #+ [string "Add some rectangles."]
 
-    -- -- handle click
-    -- onEvent eclick $ \f -> do
-    --     x <- liftIO $ do
-    --         modifyIORef count f
-    --         readIORef count
-    --     element counter # set UI.text (show x)
+    -- Button Actions
+    on UI.click clear $ const $
+        canvas # UI.clearCanvas
 
-    -- -- visual style
-    -- UI.div #. "row" #+
-    --     [ UI.ul #. "button-group round" #+
-    --         map (\x -> UI.li #+ [element x]) [bup, bdown]
-    --     , element counter #. "small-2 columns panel" 
-    --     ] #+
-    --     [element grid] #+
-    --     [element testButton]
-    
+    -- Mouse
+    out  <- UI.span # set text "Coordinates: "
+    wrap <- UI.div #. "wrap"
+        # set style [("width","300px"),("height","300px"),("border","solid black 1px")]
+        # set (attr "tabindex") "1" -- allow key presses
+        #+ [element out]
 
-    UI.div #. "grid-container" #+
+    -- Mouse detection
+    on UI.mousemove canvas $ \xy ->
+        element out # set text ("Coordinates: " ++ show xy)
+    on UI.mousedown canvas $ \(x, y) ->
+        drawSquare canvas x y 25 "Red"
+
+    getBody window #+ 
         [
-            UI.div #. "header",
-            UI.div #. "menu",
-            UI.div #. "main"#+
-                [element grid],
+            UI.div #. "page-container" #+
+                [
+                    UI.div #. "header"#+
+                        [element wrap],
+                    UI.div #. "menu"#+
+                        [element clear, 
+                        element drawRects],
+                    UI.div #. "main"#+
+                        [element canvas],
+                    UI.div #. "right",
+                    UI.div #. "footer"
 
-            UI.div #. "right",
-            UI.div #. "footer"
-
+                ]
         ]
-
-
     
+    -- draw some rectangles
+    on UI.click drawRects $ const $ do
+        let rects = [ (20 , 130, 15, 120, "teal")
+                    , (345, 110, 15, 90, "lightblue")
+                    , (220, 360, 95, 15, "teal")
+                    ]
+        forM_ rects $ \(x,y,w,h,color) -> do
+            canvas # set' UI.fillStyle (UI.htmlColor color)
+            canvas # UI.fillRect (x,y) w h
+
+    return()
+         
